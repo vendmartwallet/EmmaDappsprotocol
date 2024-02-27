37b63b00268554f9518582b6a857e9af6308496b
david <161246714+vendmartwallet@users.noreply.github.com>
2024-02-27 23:54:13 +0100

    ChatBox.jsx
    updated the email

--- src/components/chatbox/ChatBox.jsx
+++ src/components/chatbox/ChatBox.jsx
@@ -5,7 +5,7 @@ const ChatBox = () => {
   return (
     <div className="fixed bottom-8 right-6 bg-[#fff] bg-transparent rounded-full flex items-center justify-center p-2">
     <div className="animate-float">
-      <a href="mailto:dappswalletsup@gmail.com">
+      <a href="mailto:Lostapeclub@gmail.com">
       <IoMdChatboxes size="53" color="#6366F1"/>
       </a>
     </div>
